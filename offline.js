f919906da3a78f7e5de1601289ba2d83f50c1a2b
semicon <30399464+semicon@users.noreply.github.com>
2019-06-28 14:48:02 +0700

    happyglass
    happyglass

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1561706738,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/leftwall-sheet0.png",
+		"images/rightwall-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/glass-sheet0.png",
+		"images/glass-sheet1.png",
+		"images/glass-sheet2.png",
+		"images/tube-sheet0.png",
+		"images/water-sheet0.png",
+		"images/bg.png",
+		"images/ground.png",
+		"images/logo1-sheet0.png",
+		"images/logo2-sheet0.png",
+		"images/replaybtn-sheet0.png",
+		"images/backbtn-sheet0.png",
+		"images/barcounter-sheet0.png",
+		"images/barchecking-sheet0.png",
+		"images/emotion-sheet0.png",
+		"images/emotion-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
